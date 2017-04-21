00fc99a060e6025b97343f4d5d295897fe95a1de
Nicolas Martin Castano Gomez <ncastano@localhost.localdomain>
2017-04-21 11:53:41 -0300

    Falta crear clases de particulas para los metodos.

--- src/DampedHarmonicOscillator.java
+++ src/DampedHarmonicOscillator.java
@@ -27,6 +27,10 @@ public class DampedHarmonicOscillator {
         return getForce(p)/m;
     }
 
+    public double getAccelerationGPR5(double predictedX, double predictedV){
+        return -k*predictedX -gamma*predictedV;
+    }
+
     public double getForce(Particle p){
         return -k*p.x -gamma*p.vx;
     }
@@ -54,7 +58,46 @@ public class DampedHarmonicOscillator {
 
     }
 
+    public void gearPredictorCorrector5(double time){
 
+        //Primero calculo los r'
+        p.ax = getAcceleration(p);
+        double aprimera = -(k/m)*p.vx - (gamma/m)*p.ax;
+        double asegunda = -(k/m)*p.ax - (gamma/m)*aprimera;
+        double atercera =  -(k/m)*aprimera - (gamma/m)*asegunda;   //Todo esto vale para el paso inicial!! Dsps me baso en los predichos
+
+        //Creo la nueva particula para setearle los r' predichos
+        p.next = new Particle(m,1);
+
+
+        //Predecir los r'
+        Particle predicted = new Particle(0, 1, m);
+
+        predicted.x = p.x + p.vx*time + p.ax*((time*time)/2) + aprimera*((time*time*time)/6) + asegunda*((time*time*time*time)/24) + atercera*((time*time*time*time)/120);
+        predicted.vx = p.vx + p.ax*time + aprimera*((time*time)/2) + asegunda*((time*time*time)/6) + atercera*((time*time*time*time)/24);
+        predicted.ax = p.ax + aprimera*time + asegunda*((time*time)/2) + atercera*((time*time*time)/6);
+        double aprimeraPredicha = aprimera + asegunda*time + atercera*((time*time)/2);
+        double asegundaPredicha = asegunda + atercera*time;
+        double aterceraPredicha = atercera;  //Hace falta?
+
+        //Calcular el factor de correcion DELTA R2
+        double factorCorreccion = (getAccelerationGPR5(predicted.x, predicted.vx) - predicted.ax)*((time*time)/2);
+
+
+        //Calcular los r' corregidos
+        p.next.x = predicted.x + (3.0/16.0)*factorCorreccion;
+        p.next.vx = predicted.vx + (251/360)*(factorCorreccion/time);
+        p.next.ax = predicted.ax + (factorCorreccion/(time*time))*2;
+
+        //Actualizo la particula
+        p.previous.x  = p.x;
+        p.previous.vx = p.vx;
+        p.previous.ax = p.ax;
+
+        p.x = p.next.x;
+        p.vx = p.next.vx;
+        p.ax = p.next.ax;
+    }
 
 
 
